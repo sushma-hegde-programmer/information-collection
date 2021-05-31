0b7601dc1923c140ea47c67bcbccb088a1660194
sushma-hegde-programmer <sushma.h@careator.com>
2021-06-01 01:51:04 +0530

    drawer is added and is working fine

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { Button } from "antd";
 import PageNavbar from "./components/PageNavBar";
 import PageLayout from "./containers/PageLayout";
 import ContentPage from "./components/ContentPage";
+import PageDrawer from "./components/PageDrawer";
 import "./App.css";
-
-import Axios from "axios";
 
 function App() {
   return (
     <div>
+      <PageDrawer />
       <PageLayout>
         <ContentPage />
       </PageLayout>
--- src/components/PageAppBar.tsx
+++ src/components/PageAppBar.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Layout, Menu } from "antd";
 import {
-  UserOutlined,
+  DashboardOutlined,
+  HomeOutlined,
   LaptopOutlined,
   NotificationOutlined,
 } from "@ant-design/icons";
@@ -19,23 +20,17 @@ function PageAppBar() {
           defaultOpenKeys={["sub1"]}
           style={{ height: "100%" }}
         >
-          <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-            <Menu.Item key="1">option1</Menu.Item>
-            <Menu.Item key="2">option2</Menu.Item>
-            <Menu.Item key="3">option3</Menu.Item>
-            <Menu.Item key="4">option4</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-            <Menu.Item key="5">option5</Menu.Item>
-            <Menu.Item key="6">option6</Menu.Item>
-            <Menu.Item key="7">option7</Menu.Item>
-            <Menu.Item key="8">option8</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-            <Menu.Item key="9">option9</Menu.Item>
-            <Menu.Item key="10">option10</Menu.Item>
-            <Menu.Item key="11">option11</Menu.Item>
-            <Menu.Item key="12">option12</Menu.Item>
+          <Menu.Item icon={<HomeOutlined />} key="1" title="Home">
+            Home
+          </Menu.Item>
+          <Menu.Item key="sub1" icon={<DashboardOutlined />} title="Dashboard">
+            Dashboard
+          </Menu.Item>
+          <SubMenu key="sub2" icon={<LaptopOutlined />} title="Details">
+            <Menu.Item key="3">My work</Menu.Item>
+            <Menu.Item key="6">To do</Menu.Item>
+            <Menu.Item key="7">option2</Menu.Item>
+            <Menu.Item key="8">option3</Menu.Item>
           </SubMenu>
         </Menu>
       </Sider>
