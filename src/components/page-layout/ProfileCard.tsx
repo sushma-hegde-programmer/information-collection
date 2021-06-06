8ad8544a4aa4888539e5fd012e4fc86018a422da
sushma-hegde-programmer <sushma.h@careator.com>
2021-06-06 11:59:24 +0530

    before deleting profile card

--- src/components/page-layout/ProfileCard.tsx
+++ src/components/page-layout/ProfileCard.tsx
@@ -6,6 +6,7 @@ import React from "react";
 function ProfileCard() {
   return (
     <div className="card">
+      {/* when user clicks on profile what should get displayed */}
       <Card>
         <div className="card__options">
           <Button shape="round">View Profile</Button>
